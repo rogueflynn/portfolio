faa3b262fbdce1bec3461a3418e33e9c949f0697
rogueflynn <vgvgonzalez8@gmail.com>
2018-01-06 17:30:16 -0500

    Added laptop screen and animation

---
+++ src/screenText.js
@@ -0,0 +1,37 @@
+
+export const screenText = (index) => {
+        const message = [ "P",
+        "Pr",
+        "Pro",
+        "Prog",
+        "Progr",
+        "Progra",
+        "Program",
+        "Programm",
+        "Programme",
+        "Programmer\n\n",
+        "Programmer\n\nD",
+        "Programmer\n\nDe",
+        "Programmer\n\nDev",
+        "Programmer\n\nDeve",
+        "Programmer\n\nDevel",
+        "Programmer\n\nDevelo",
+        "Programmer\n\nDevelop",
+        "Programmer\n\nDevelope",
+        "Programmer\n\nDeveloper",
+        "Programmer\n\nDeveloper\n\nP",
+        "Programmer\n\nDeveloper\n\nPr",
+        "Programmer\n\nDeveloper\n\nPro",
+        "Programmer\n\nDeveloper\n\nProb",
+        "Programmer\n\nDeveloper\n\nProbl",
+        "Programmer\n\nDeveloper\n\nProble",
+        "Programmer\n\nDeveloper\n\nProblem",
+        "Programmer\n\nDeveloper\n\nProblem S",
+        "Programmer\n\nDeveloper\n\nProblem So",
+        "Programmer\n\nDeveloper\n\nProblem Sol",
+        "Programmer\n\nDeveloper\n\nProblem Solv",
+        "Programmer\n\nDeveloper\n\nProblem Solve",
+        "Programmer\n\nDeveloper\n\nProblem Solver"
+    ];
+    return message[index];
+}
